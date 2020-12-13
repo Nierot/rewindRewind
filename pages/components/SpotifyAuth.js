5c5dd531b2e339b0c909b27f9e77bc13c8c5b7ad
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-13 20:28:30 +0100

    oof bad practises

--- pages/components/SpotifyAuth.js
+++ pages/components/SpotifyAuth.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { redirect_uri, client_id } from '../../settings.json';
 
 export default class extends React.Component {
   generateLink = () => {
     let params = new URLSearchParams();
-    params.set('client_id', client_id)
+    params.set('client_id', process.env.NEXT_PUBLIC_CLIENT_ID)
     params.set('response_type', 'token')
-    params.set('redirect_uri', redirect_uri)
+    params.set('redirect_uri', process.env.NEXT_PUBLIC_REDIRECT_URI)
     params.set('scope', '')
     return `https://accounts.spotify.com/authorize?${params.toString()}`
   }
