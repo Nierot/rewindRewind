fcab6fdcacaa13d9ab8b79f8c4b27cfc8bb1066b
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-13 20:35:52 +0100

    fixed redirect uri

--- pages/components/SpotifyAuth.js
+++ pages/components/SpotifyAuth.js
@@ -5,7 +5,7 @@ export default class extends React.Component {
     let params = new URLSearchParams();
     params.set('client_id', process.env.NEXT_PUBLIC_CLIENT_ID)
     params.set('response_type', 'token')
-    params.set('redirect_uri', process.env.NEXT_PUBLIC_REDIRECT_URI)
+    params.set('redirect_uri', window.location.protocol + '//' + window.location.host + '/')
     params.set('scope', '')
     return `https://accounts.spotify.com/authorize?${params.toString()}`
   }
