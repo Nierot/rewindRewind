bd34e00d45985f4133b1d41ec8218a7de03e1109
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-13 20:44:34 +0100

    fixed button

--- pages/components/SpotifyAuth.js
+++ pages/components/SpotifyAuth.js
@@ -1,20 +1,21 @@
 import React from 'react'
 
 export default class extends React.Component {
-  generateLink = () => {
+
+  redirect = () => {
     let params = new URLSearchParams();
     params.set('client_id', process.env.NEXT_PUBLIC_CLIENT_ID)
     params.set('response_type', 'token')
     params.set('redirect_uri', window.location.protocol + '//' + window.location.host + '/')
     params.set('scope', '')
-    return `https://accounts.spotify.com/authorize?${params.toString()}`
+    location.replace(`https://accounts.spotify.com/authorize?${params.toString()}`)
   }
   
   render() {
     return (
-      <a href={this.generateLink()} className={this.props.style}>
+      <div onClick={this.redirect} className={this.props.style}>
         Login with Spotify
-      </a>
+      </div>
     )
   }
 }
