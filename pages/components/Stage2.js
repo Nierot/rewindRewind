a58b56ccf524b2c5e71e7bc80bdc4c95b30e8d3e
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-14 21:13:23 +0100

    styling for stage 2

---
+++ pages/components/Stage2.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export default class extends Component {
+  render() {
+    console.log(this.props.playlists)
+    return (
+      <>
+        oof
+      </>
+    )
+  }
+}
