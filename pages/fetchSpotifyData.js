58f895b5fefe40e05eaf2f7daaa13b91b67813d5
Nierot <56843328+Nierot@users.noreply.github.com>
2020-12-14 21:13:00 +0100

    get all playlists

--- pages/fetchSpotifyData.js
+++ pages/fetchSpotifyData.js
@@ -1,19 +1,19 @@
 import Spotify from 'spotify-web-api-js';
 
-export default async function getData(accessToken) {
+export default async function getData() {
   const s = new Spotify();
-  let playlists = [];
   let id;
   s.setAccessToken(localStorage.getItem('spotify'))
-  s.getMe()
+  const playlists = await s.getMe()
     .then(data => id = data.id)
     .then(() => s.getUserPlaylists(id))
     .then(async data => {
-      playlists = data.items;
+      let playlists = data.items;
       while (playlists.length < data.total) {
         playlists = playlists.concat((await s.getUserPlaylists(id, { offset: playlists.length })).items)
       }
       return new Promise(r => r(playlists));
     })
-    .then(p => console.log(p));
+  return playlists;
+  // laat user alle wrapped playlists aanklikken
 }
